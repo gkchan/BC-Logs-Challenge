22d59a384b161cf113b084ded942845ad68181b4
Grace <gkchan09@gmail.com>
2017-11-14 08:13:27 +0000

    WIP: Sort log sources by date

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -4,7 +4,8 @@ module.exports = (logSources, printer) => {
 
     // Note: need to import sourceCount
 
-    const sourceCount = require('../index.js')
+    // const sourceCount = require('../index.js')
+    const sourceCount = 10
 
     // Note: syncLogSources is logSources?
 
@@ -16,48 +17,116 @@ module.exports = (logSources, printer) => {
     
     // Note: print 1 log source
 
-    var current_log
+    // var current_log
 
-    while (current_log != 'false') {
-        current_log = logSources[0].pop()
-        if (current_log === false) {
-            break
-        } else {
-            // console.log(current_log)
-            printer.print(current_log)
-        }
-    }
+    // while (current_log != false) {
+    //     current_log = logSources[0].pop()
+    //     if (current_log === false) {
+    //         break
+    //     } else {
+    //         // console.log(current_log)
+    //         printer.print(current_log)
+    //     }
+    // }
+
+    // Thoughts:
+    // merge sort
+    // pop first entry into list, hashmap
+            // sort it?
+    // keep track of earliest date, logSources
+    // keep track of second earliest date?
+    // replace entry with next entry of same log source
 
     
+    var earliestLogs = []
+    var earliestDate = ""
+
+    for (var i = 0; i < sourceCount; i++) {  
+        var log = logSources[i].pop()
+        if (earliestDate === "" | log.date < earliestDate) {
+            earliestDate = log.date
+        }
+        var log_dict = {log, i}
+        earliestLogs.push(log_dict)
+        // console.log(earliestLogs)
+        for (var j = i-1; j >=0; j--) {
+            // console.log(earliestLogs[j].log.date)
+            if (log.date < earliestLogs[j].log.date) {
+                // console.log("swap")
+                earliestLogs[j+1] = earliestLogs[j]
+                earliestLogs[j] = log_dict
+
+            }
+
+        }
+        
+    }   
+    
+    console.log(earliestLogs, earliestDate)
+
+    console.log(earliestDate)
+
+    // earliestLogs.log.date.sort()   
+    // console.log(earliestLogs)
+
+    // console.log(logSources[0].last,logSources[1].last)
+    // console.log(logSources[0].last.date > logSources[1].last.date)
+
+
+
+
     // var earliestLogs = []
     // for (var i = 0; i < 1; i++) {  
 
-        // printer.print(logSources[i].pop())
+    //     // printer.print(logSources[i].pop())
     
     //     var log = logSources[i].pop()
     //     earliestLogs.push({log, i})
     //     earliestLogs.sort()    
     // }   
 
+    // var log_index, log
+
     // console.log(earliestLogs)
-    // while (logSources != []) {
 
-        // console.log(3)
-        // console.log(earliestLogs[0])
+    // // while (logSources != []) {
+    // while (4===4) {
 
-    //     var log_index = earliestLogs[0].i
+
+  
+    //     // console.log(earliestLogs[0])
+
+    //     log_index = earliestLogs[0].i
+    //     if (log_index === 1) {
+    //         console.log("error")
+    //     }
     //     printer.print(earliestLogs[0].log)
 
-    //     var current_log = logSources[log_index].pop()
-    //     if (current_log === false) {
-    //         logSources.splice(i,1) 
-    //         // console.log(current_log, 1)
+    //     earliestLogs[log_index] = logSources[log_index].pop()
+    //     // earliestLogs.splice(0, 1)
+
+    //     log = logSources[log_index].pop()
+    //     if (log === false) {
+    //         logSources.splice(i,1)
+    //         console.log("delete") 
+    //         break
+    //         // console.log(logSources)
+    //         // console.log(log, 1)
+    //         // earliestLogs.sort()
+    //         // console.log("E", earliestLogs)
     //     } else {
-    //         earliestLogs.push({current_log, i})
+    //         earliestLogs.push({log, i})
+    //         // console.log("E", earliestLogs)
+    //         // if (i === 1) {
+    //         //     console.log("error")
+    //         // }
+
     //         earliestLogs.sort()
-    //         // console.log(current_log, 2)
+    //         // console.log(log, 2)
     //     }
     // }
+
+
 
     // if (earliestLogs === []) {
         
